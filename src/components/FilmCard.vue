9a9fd8b45e19328224f24ec7ac11bef2b00129fc
ludoff <ludovicofiore25@gmail.com>
2024-07-12 11:19:29 +0200

    feat: substitute string for language with image (for movies)

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -3,7 +3,47 @@
 export default {
     name: 'FilmCard',
     props: ['movieData'],
+
+    data() {
+        return {
+            
+            // struttura dati per immagini lingua
+            langImg: [
+                {
+                    language: 'en',
+                    image: '../src/assets/img/Flag-United-Kingdom.webp',
+                    // image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg/1200px-Flag_of_the_United_Kingdom_%281-2%29.svg.png'
+                },
+                {
+                    language: 'de',
+                    image: '../src/assets/img/germanyflag__35169.jpg',
+                },
+                {
+                    language: 'it',
+                    image: '../src/assets/img/italy-flag.png',
+                },
+                {
+                    language: 'ja',
+                    image: '../src/assets/img/jp-flag.png',
+                },
+                {
+                    language: 'fr',
+                    image: '../src/assets/img/Flag_of_France.svg.webp',
+                },
+            ],
+        }
+    },
+
+    computed: {
+        // computed per immagine lingua
+        matchedImage() {
+            return this.langImg.find(imageFlag => imageFlag.language === this.movieData.original_language);
+        }
+    },
+
 }
+
+
 </script>
 
 <template>
@@ -12,7 +52,11 @@ export default {
 
         <h5>{{ movieData.original_title }}</h5>
 
-        <p>{{ movieData.original_language }}</p>
+        <div class="img-container">
+            <img v-if="matchedImage" :src="matchedImage.image" :alt="matchedImage.language" />
+            <p v-else>{{ movieData.original_language }}</p>
+        </div>
+        
 
         <p>{{ movieData.vote_average }}</p>
     </div>
@@ -25,5 +69,13 @@ export default {
     background-color: aquamarine;
     border: 1px solid black;
     text-align: center;
+
+    .img-container {
+        width: 100%;
+
+        img {
+            max-width: 50%;
+        }
+    }
 }
 </style>
