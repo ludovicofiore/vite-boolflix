844bdbde6ef77326d119e20917031f1882eab20a
ludoff <ludovicofiore25@gmail.com>
2024-07-15 10:56:34 +0200

    feat: add hover effect for cards fix: fix api url for italian language

--- src/store.js
+++ src/store.js
@@ -5,11 +5,11 @@ export const store = reactive({
 
     // api per film
     filmArray: [],
-    apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=e9b0d0fd3b1205382e395e5b00212d57&language=it_IT',
+    apiURL: 'https://api.themoviedb.org/3/search/movie?language=it-IT&api_key=e9b0d0fd3b1205382e395e5b00212d57',
 
     // api per serie tv
     tvArray: [],
-    apiURLTv: 'https://api.themoviedb.org/3/search/tv?api_key=e9b0d0fd3b1205382e395e5b00212d57&language=it_IT',
+    apiURLTv: 'https://api.themoviedb.org/3/search/tv?language=it-IT&api_key=e9b0d0fd3b1205382e395e5b00212d57',
 
     // query da aggiungere ad api
     filmQuery: '',
